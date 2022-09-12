3b42bd618b9212b83b633cafaacaa81a5d6cf7d6
fernandolop <lopez_studio@hotmail.com>
2022-09-12 14:21:17 -0300

    Delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config && \
-service ssh restart && \
-rm -f wp-config.php && \
-wget https://raw.githubusercontent.com/fernandolop/vansur/main/wp-config.php && \
